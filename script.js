51c202b88564a8aea8463212b65773e398884e80
Sean Walters <skw19001@gmail.com>
2022-10-03 18:07:10 -0400

    Create mouseover effect to change backgroun color

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const container = document.querySelector('#container');
 const content = document.createElement('div');
-container.setAttribute('style', 'display: flex; justify-content: space-evenly; flex-wrap: wrap; width: 48px; margin: auto;');
-content.setAttribute('style', 'height: 1px; width: 1px; background-color: #c0c0c0; border: 1px solid #000000;')
+content.classList.add('grid')
+container.setAttribute('style', 'display: flex; justify-content: space-evenly; flex-wrap: wrap; width: 1312px; margin: auto;');
+//content.setAttribute('style', 'height: 50px; width: 80px; background-color: #c0c0c0; border: 1px solid #000000;')
 
 for(let i = 0; i < 16; i++) {
     for(let j = 0; j < 16; j++) {
         container.appendChild(content.cloneNode(true));
     }
 }
+
+const grids = document.querySelectorAll('.grid');
+grids.forEach((grid) => {
+    grid.addEventListener('mouseover', () => {
+        grid.setAttribute('style', 'background-color: red;')
+    })
+})
